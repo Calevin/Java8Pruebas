53b8a48a5dd6303a303768cd7c8d59942a525890
Sebastian Barreto <sebastian.e.barreto@gmail.com>
2019-10-15 03:20:38 -0300

    Fix LocalDate
     Revisar rangos...

--- Ejemplos/src/calevin/fecha/TestLocalDate.java
+++ Ejemplos/src/calevin/fecha/TestLocalDate.java
@@ -1,6 +1,8 @@
 package calevin.fecha;
 
 import java.time.LocalDate;
+import java.time.Period;
+import java.util.stream.IntStream;
 
 public class TestLocalDate {
 
@@ -8,14 +10,51 @@ public class TestLocalDate {
 		
 		LocalDate hoy = LocalDate.now();
 		System.out.println("Hoy: " + hoy);
-		LocalDate cumpleaños = LocalDate.of(hoy.getYear(), 9, 10);
-		System.out.println("Cumpleaños: " + cumpleaños);
+		LocalDate cumpleanios = LocalDate.of(hoy.getYear(), 9, 10);
+		System.out.println("Cumpleaños: " + cumpleanios);
 		System.out.println("Mes actual (nombre): " + hoy.getMonth());
 		System.out.println("Mes actual: (numero)" + hoy.getMonthValue());
 		System.out.println("Dia actual: " + hoy.getDayOfMonth());
 		
-		System.out.println("Hoy es anterior a mi cumpleaños? " + hoy.isBefore(cumpleaños));
-		LocalDate cumpleañosAñoNacimiento = cumpleaños.withYear(1987);
-		System.out.println("Cumpleaños (año de nacimiento): " + cumpleañosAñoNacimiento);
+		System.out.println("Hoy es anterior a mi cumpleaños? " + hoy.isBefore(cumpleanios));
+		LocalDate cumpleañosAnioNacimiento = cumpleanios.withYear(1987);
+		System.out.println("Cumpleaños (año de nacimiento): " + cumpleañosAnioNacimiento);
+		System.out.println("Cumpleaños + 10 años: " + cumpleañosAnioNacimiento.plusYears(10));
+		System.out.println("Cumpleaños (año de nacimiento): " + cumpleañosAnioNacimiento);
+		System.out.println("Hoy + 1 año, 2 mes, 3 dia: " 
+				+ hoy.plusYears(1).plusMonths(2).plusDays(3));
+		
+		Period periodo = Period.between(cumpleañosAnioNacimiento, hoy);
+		System.out.println("Años " + periodo.getYears());
+		System.out.println("Meses " + periodo.getMonths());
+		System.out.println("Dias " + periodo.getDays());
+		
+		//ejemploRango();
+	}
+
+	private static void ejemploRango() {
+		//System.out::println
+		//.anyMatch
+		LocalDate cumpleañosAnioNacimiento = LocalDate.of(1987, 9, 10);
+		System.out.println("forEach ----------------------------------------------------");
+		IntStream.range(1, 32).forEach(n -> {
+			System.out.println(n + ") Cumpleaños " + cumpleañosAnioNacimiento.plusYears(n));
+		});
+		System.out.println("anyMatch ----------------------------------------------------");
+		IntStream.range(1, 32).anyMatch(n -> {
+			System.out.println(n + ") Cumpleaños " + cumpleañosAnioNacimiento.plusYears(n));
+			return cumpleañosAnioNacimiento.plusYears(n).getYear() == 2001;
+		});
+		System.out.println("filter ----------------------------------------------------");
+		IntStream.range(1, 32).filter(n -> {
+			System.out.println(n + ") Cumpleaños " + cumpleañosAnioNacimiento.plusYears(n));
+			return cumpleañosAnioNacimiento.plusYears(n).getYear() == 2001;
+		})
+		.forEach(n -> {
+			System.out.println(n + ")(Otro Stream) Cumpleaños " + cumpleañosAnioNacimiento.plusYears(n));
+		});
+		System.out.println("----------------------------------------------------");
+				
+		
 	}
 }
