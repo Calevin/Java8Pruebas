9337f6ac8ca92e642aee8f2b451239e757e46bb3
Sebastian Barreto <sebastian.e.barreto@gmail.com>
2019-10-21 01:53:44 -0300

    Optional

---
+++ Ejemplos/src/calevin/optional/TestOpcionalMain.java
@@ -0,0 +1,40 @@
+package calevin.optional;
+
+import java.util.Optional;
+
+import entidades.Empleado;
+
+public class TestOpcionalMain {
+
+	public static void main(String[] args) {
+
+		Optional<Empleado> empleadoEncontrado = buscarEmpleadoPorNombre("nadie");
+		
+		System.out.println(empleadoEncontrado.isPresent()?empleadoEncontrado.get():"No encontrado");
+		
+		empleadoEncontrado = buscarEmpleadoPorNombre("Seba");
+		
+		System.out.println(empleadoEncontrado.isPresent()?empleadoEncontrado.get():"No encontrado");
+		
+	}
+
+	public static Optional<Empleado> buscarEmpleadoPorNombreSinStream(String nombre){
+		Optional<Empleado> empleadoEncontrado = Optional.empty();
+		
+		for (Empleado e : Empleado.empleadosParaTest()) {
+			if(e.getNombre().equalsIgnoreCase(nombre)) {
+				empleadoEncontrado = Optional.of(e);
+			}
+			
+		}
+		
+		return empleadoEncontrado;
+	}
+	
+	public static Optional<Empleado> buscarEmpleadoPorNombre(String nombre){
+		return Empleado.empleadosParaTest()
+				.stream()
+				.filter(p -> p.getNombre().equalsIgnoreCase(nombre))
+				.findFirst();
+	}
+}
